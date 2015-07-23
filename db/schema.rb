d2f38fb4545743a61d43c9a0b56241364a9c4c99
NEKOTENI <riverfield.tk@gmail.com>
2015-07-23 09:12:35 +0900

    create User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150722000708) do
+ActiveRecord::Schema.define(version: 20150723000637) do
 
   create_table "memos", force: :cascade do |t|
     t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "mail"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
